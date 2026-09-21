00eae60ac7cfd8866fce4ca7418ac3afbe0dd97a
vhugolm2023 <vhugolm2023@gmail.com>
2026-09-21 23:42:23 +0200

    fix en .env hugo

--- src/gestiondebiblioteca/ConexionBD.java
+++ src/gestiondebiblioteca/ConexionBD.java
@@ -7,17 +7,17 @@ package gestiondebiblioteca;
 
     public class ConexionBD {
 
-        
+
         private static final Dotenv dotenv = Dotenv.load();
 
-       
+
         private static final String URL = dotenv.get("DB_URL");
         private static final String USER = dotenv.get("DB_USER");
         private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
         public static Connection getConnection() throws SQLException {
             try {
-               
+
                 return DriverManager.getConnection(URL, USER, PASSWORD);
             } catch (SQLException e) {
                 System.out.println("Error al conectar a la base de datos: " + e.getMessage());
